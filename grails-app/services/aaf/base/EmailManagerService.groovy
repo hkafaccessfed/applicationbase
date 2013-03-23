7bec1110ea31c9062db9c90cbd1011551ea7b8cb
Bradley Beddoes <bradleybeddoes@gmail.com>
2013-03-23 11:03:31 +1000

    Email: Support sending email outside a web request.
    Adds a fake HttpRequest for quartz job and non request threads so
    that they can successfully send email.
    
    Without this groovyPagesTemplateEngine throws exceptions.

--- grails-app/services/aaf/base/EmailManagerService.groovy
+++ grails-app/services/aaf/base/EmailManagerService.groovy
@@ -2,12 +2,25 @@ package aaf.base
 
 import aaf.base.admin.EmailTemplate
 
+import org.springframework.web.context.request.RequestContextHolder
+import org.codehaus.groovy.grails.web.context.ServletContextHolder
+import org.springframework.web.context.support.WebApplicationContextUtils
+
 class EmailManagerService {
   def mailService
   def groovyPagesTemplateEngine
   def groovyPageRenderer
 
   public void send(String _to, String _subject, EmailTemplate view, Map model, String _cc = null, String _bcc = null) {
+
+    // groovyPagesTemplate requires request object
+    // create one if we're calling from quartz etc which doesn't have a request bound
+    def webRequest = RequestContextHolder.getRequestAttributes()
+    if(!webRequest) {
+      def servletContext  = ServletContextHolder.getServletContext()
+      def applicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext)
+      webRequest = grails.util.GrailsWebUtil.bindMockWebRequest(applicationContext)
+    }
 
     def output = new StringWriter()
     groovyPagesTemplateEngine.createTemplate(view.content, view.name).make(model).writeTo(output)
